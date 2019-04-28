e12a16cb6139c77df0581f57602d5b2e4fee806d
Geckley <techncially.geckley@gmail.com>
2019-04-28 02:51:05 -0500

    set up static producition build

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-Parser");
 const passport = require("passport");
+const path = require("path");
 
 const auth = require("./routes/api/auth");
 
@@ -31,6 +32,16 @@ require("./config/passport")(passport);
 
 // Use routes
 app.use("/api/auth", auth);
+
+// server static assets if in production
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
